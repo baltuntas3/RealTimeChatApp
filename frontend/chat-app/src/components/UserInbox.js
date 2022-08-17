473de1c443a1b176dd4b3d5778299ab9d4731b24
burak <burak.altuntas@yandex.com>
2022-08-17 22:17:56 +0300

    Add UserInbox

--- frontend/chat-app/src/components/UserInbox.js
+++ frontend/chat-app/src/components/UserInbox.js
@@ -8,5 +8,27 @@ import Inbox from './Inbox';
 
 export default function UserInbox(){
 
- 
+  const [messages,setMessages]=useState([])
+
+  async function getInbox(){
+    const getInbox = await axios.get("users/inbox")
+    setMessages(getInbox.data)
+  }
+
+  useEffect(() => {
+    getInbox()       
+  }, []);
+
+    
+  return (
+    <div>
+      Inbox Component
+      {messages.map(data=><div key={data._id}>
+        {data._id}
+        {data.participants.map(user=><div key={user._id}>{user.userName}</div>)}
+      </div>)}
+
+
+    </div>
+  )
 }
